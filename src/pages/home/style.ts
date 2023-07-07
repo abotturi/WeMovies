fe072aada53bcb952e45120ac7d69534d32babc1
abotturi <60594258+abotturi@users.noreply.github.com>
2023-07-07 11:46:30 -0300

    add grid improvements

--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -3,19 +3,7 @@ import { styled } from "styled-components";
 export const HomeContainer = styled('div')`
     display: grid;
     grid-gap: 20px;
-    grid-template-columns: repeat(4, 1fr);
-
-    @media (max-width: 1850px) {  
-        grid-template-columns: repeat(3, 1fr);
-    }
-
-    @media (max-width: 1380px) {  
-        grid-template-columns: repeat(2, 1fr);
-    }
-
-    @media (max-width: 900px) {  
-        grid-template-columns: repeat(1, 1fr);
-    }
+    grid-template-columns: repeat(auto-fill, minmax(290px, 1fr));
 `
 
 export const ItemContainer = styled('div')`
